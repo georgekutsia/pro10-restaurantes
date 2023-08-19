73c4e7b1fac58c90b42dbef78f4faeac2c28cf67
George <georgekutsia@gmail.com>
2023-08-19 03:40:42 +0100

    Perfil, navbar, rutas y muchas movidas

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RestaurantI, UserI } from 'src/app/models/interfaces';
+import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Component({
   selector: 'app-profile',
@@ -6,17 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit{
-  usuario: any; 
-
-  constructor() { }
+  usuario!: UserI; 
+  id!: string;
+  favorite!: any;
+  constructor(private authUser: AuthService, private activatedRoute: ActivatedRoute, private router: Router) { }
   ngOnInit(): void {
-    this.usuario = JSON.parse(localStorage.getItem('user') || '{}');
+    this.activatedRoute.paramMap.subscribe(params => {
+      this.id = String(params.get('id'));
+    })
+    this.authUser.getUserById(this.id).subscribe((data: any) => {
+      this.usuario = { ...data }
+      this.favorite = this.usuario.favorite;
+      console.log("eeee", this.usuario)
+    })
+    // this.usuario = JSON.parse(localStorage.getItem('user') || '{}');
     const reloadFlag = localStorage.getItem('reloadFlag');
     if (reloadFlag === 'true') {
       localStorage.removeItem('reloadFlag'); 
       location.reload();
     }
-    console.log(this.usuario)
+    console.log()
   }
 
   
